3314c77fadd8c6c775dc6fabccf24848516f02e4
daikiiin <dannderion@icloud.com>
2021-04-06 18:03:17 +0900

    投稿機能の実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :training_status
+
+  with_options presence: true do
+    validates :name                 
+    validates :explain           
+  end
+  validates :training_status_id, numericality: { other_than: 1 }
 end

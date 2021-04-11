6681463a6417fbe519c899bbdb97e8ddcf057dd1
daikiiin <dannderion@icloud.com>
2021-04-12 00:45:26 +0900

    deviseのビューファイル作成

--- db/schema.rb
+++ db/schema.rb
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 2021_04_11_151003) do
     t.integer "training_status_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|

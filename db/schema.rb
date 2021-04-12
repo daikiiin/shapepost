f2921c3ed62fe3530784c2e05c7d42f877e42f71
daikiiin <dannderion@icloud.com>
2021-04-12 11:44:42 +0900

    postテーブルとuserテーブルのreference設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_11_151003) do
+ActiveRecord::Schema.define(version: 2021_04_07_055042) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -37,9 +37,9 @@ ActiveRecord::Schema.define(version: 2021_04_11_151003) do
     t.string "name", null: false
     t.text "explain", null: false
     t.integer "training_status_id", null: false
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -63,4 +63,5 @@ ActiveRecord::Schema.define(version: 2021_04_11_151003) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "posts", "users"
 end

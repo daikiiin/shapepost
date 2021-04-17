b857959ef2ce3fe108684ea0270707a74beaa045
daikiiin <dannderion@icloud.com>
2021-04-17 17:45:58 +0900

    編集機能のルーティング設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:index, :new ,:create]
+  before_action :authenticate_user!, only: [:index, :new ,:create,:edit, :update]
   def index
     @posts = Post.all
   end
@@ -9,14 +9,21 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     if @post.save
-      redirect_to root_path
+      redirect_to authenticated_root_path
     else
       render :new
     end
   end
+  def edit
+    @post = Post.find(paramas[:id])
+  end
+  def update
+    post = Post.find(paramas[:id])
+    post.update(post_params)
+  end
 
   private
   def post_params
-    params.require(:post).permit(:name, :explain, :training_status_id, :image)
+    params.require(:post).permit(:name, :explain, :training_status_id, :image).merge(user_id: current_user.id)
   end
 end

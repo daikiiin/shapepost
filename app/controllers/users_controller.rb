7d7ed7586954e618e8a8cde1724901774a84ab4f
daikiiin <dannderion@icloud.com>
2021-04-18 22:57:31 +0900

    マイページの微修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     post = user.posts
     @nickname = user.nickname
+    @height = user.height
+    @weight = user.weight
+    @age = user.age
+    @user_sex = user.sex
+    @user_goal = user.training_goal
     @posts = post.order(id: "DESC")
   end
 end
